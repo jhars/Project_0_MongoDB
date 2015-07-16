3e9a4cb21f04f538bdff5df091b0c1d19fdde694
Jon Harlan <jonharlan@JONATHANs-MacBook-Pro-2.local>
2015-07-15 17:51:12 -0700

    removed comments

--- server.js
+++ server.js
@@ -34,16 +34,6 @@ app.get('/api/forumposts', function (req, res){
 	});
 });
 
-
-
-// //---------Show User based on ID in URL----------//
-// app.get('/api/blogposts/:id', function (req, res){
-// 	var targetID = req.params.id;
-// 	var showBlogPost= _.findWhere(blogposts, {id: targetID});
-// 	res.json(showBlogPost);
-// });
-
-
 app.get('/api/forumposts/:id', function (req, res){
 	var targetID = req.params.id;
 	ForumPost.findOne({_id: targetID}, function (err, foundForumPost){
@@ -56,28 +46,6 @@ app.get('/api/forumposts/:id', function (req, res){
 		}
 	});
 });
-
-
-//-----------get a single post (SOLUTION)-------------//
-// app.get('/api/posts/:id', function(req, res) {
-
-//   // take the value of the id from the url parameter
-//   // note that now we are NOT using parseInt
-//   var targetId = req.params.id
-
-//   // find item in database matching the id
-//   Post.findOne({_id: targetId}, function(err, foundPost){
-//     console.log(foundPost);
-//     if(err){
-//       console.log("error: ", err);
-//       res.status(500).send(err);
-//     } else {
-//       // send back post object
-//       res.json(foundPost);
-//     }
-//   });
-
-// });
 
 
 
@@ -123,15 +91,6 @@ app.put('/api/forumposts/:id', function (req, res){
 	});
 });
 
-//----------DELETE USER (ORIGINAL)-----------------//
-// app.delete('/api/blogposts/:id',function (req, res){
-// 	var targetID = req.params.id;
-// 	var foundBlogPost = _.findWhere(blogposts, {id: targetID});
-// 	var index = blogposts.indexOf(foundBlogPost);
-// 	blogposts.splice(index,1);
-// 	res.json(foundBlogPost.username + ' has been deleted');
-// });
-
 //-----------------DELETE User By ID------------------//
 app.delete('/api/forumposts/:id', function (req, res){
 	var targetID = req.params.id;
@@ -144,27 +103,6 @@ app.delete('/api/forumposts/:id', function (req, res){
 			};
 		});
 });
-
-
-
-
-// // delete post
-app.delete('/api/posts/:id', function(req, res) {
-
-  // take the value of the id from the url parameter
-  var targetId = req.params.id;
-
- // remove item from the db that matches the id
-   Post.findOneAndRemove({_id: targetId}, function (err, deletedPost) {
-    if (err){
-      res.status(500).send(err);
-    } else {
-      // send back deleted post
-      res.json(deletedPost);
-    }
-  });
-});
-
 
 // --------------listen on port 3000-------------------//
 app.listen(3000, function () {
